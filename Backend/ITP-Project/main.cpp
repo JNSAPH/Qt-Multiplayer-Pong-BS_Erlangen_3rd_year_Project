e3a63a8220314c5d152f9ea7c24a9ba74ffb5bab
Jonas <JNSAPH@icloud.com>
2022-12-20 17:19:18 +0100

    Switched to QMake

--- Backend/ITP-Project/main.cpp
+++ Backend/ITP-Project/main.cpp
@@ -1,9 +1,10 @@
 #include <QCoreApplication>
+
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
-    //QCoreApplication a(argc, argv);
-    std::cout << "Hello";
-    //return a.exec();
+    QCoreApplication a(argc, argv);
+
+    return a.exec();
 }
