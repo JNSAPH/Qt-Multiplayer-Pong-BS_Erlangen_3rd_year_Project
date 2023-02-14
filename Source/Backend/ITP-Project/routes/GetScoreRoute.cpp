aa06e042dd158eb9f213f9ea5a3dcdf0926b9ad3
Jonas <JNSAPH@icloud.com>
2023-02-14 12:47:17 +0100

    Hotfix \o/

--- Source/Backend/ITP-Project/routes/GetScoreRoute.cpp
+++ Source/Backend/ITP-Project/routes/GetScoreRoute.cpp
@@ -4,9 +4,26 @@ GetScoreRoute::GetScoreRoute() { }
 
 void GetScoreRoute::handleRoute(QTcpSocket *socket, const QMap<QString, QString> &parameters)
 {
-    // Prepare JSON Map for Dat
+    // Prepare JSON Map for Data
     QJsonArray jsonArray;
     QList<QStringList> data = Score::getScore();
+
+    // Filter out empty lines
+    /**
+     * This is stupid and i need to figure out why it randomly adds lines with missing Rows.
+     * It works tho :D
+     */
+    data.erase(std::remove_if(data.begin(), data.end(), [](const QStringList& fields) {
+        return fields[0].isEmpty() || fields[1].isEmpty() || fields[2].isEmpty();
+    }), data.end());
+
+    // Sort data by score in descending order
+    std::sort(data.begin(), data.end(), [](const QStringList& a, const QStringList& b) {
+        return a[2].toInt() > b[2].toInt();
+    });
+
+
+    // Append top 5 scores to JSON array
     int count = 0;
     for (const auto& row : data) {
         QJsonObject jsonObject;
@@ -19,12 +36,15 @@ void GetScoreRoute::handleRoute(QTcpSocket *socket, const QMap<QString, QString>
             break;
         }
     }
+
+    // Create JSON response
     QJsonObject topLevelObject;
     topLevelObject["Top5Scores"] = jsonArray;
     QJsonDocument jsonDocument(topLevelObject);
-
     std::string response = WSUtils::createJSONResponse(jsonDocument.toJson().toStdString());
 
+    // Send response and close socket
     socket->write(response.c_str());
     socket->close();
 }
+
